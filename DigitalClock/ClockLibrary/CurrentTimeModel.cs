201d0a55d7e858191b765c134ec317348048bd3c
luki532 <lukisaw01@gmail.com>
2022-02-13 14:12:23 +0100

    First commit

---
+++ DigitalClock/ClockLibrary/CurrentTimeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockLibrary
+{
+    public class CurrentTimeModel
+    {
+        public string CurrentTime { get; set; }
+
+        public async Task GetCurrentTimeAsync()
+        {
+            CurrentTime = DateTime.Now.ToString("HH:mm:ss");
+            await Task.Delay(1000);
+        }
+
+        public async Task UpdateTimeAsync()
+        {
+            await GetCurrentTimeAsync();
+        }
+    }
+}
