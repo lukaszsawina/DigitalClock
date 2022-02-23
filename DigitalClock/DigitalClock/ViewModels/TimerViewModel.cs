c075501f80e5f74abe83f07439f647f2883e9c8b
luki532 <lukisaw01@gmail.com>
2022-02-23 22:44:56 +0100

    Timer

--- DigitalClock/DigitalClock/ViewModels/TimerViewModel.cs
+++ DigitalClock/DigitalClock/ViewModels/TimerViewModel.cs
@@ -10,10 +10,18 @@ namespace DigitalClock.ViewModels
 {
     class TimerViewModel : Screen
     {
+        IWindowManager manager = new WindowManager();
         private string _timer = "00:00:00";
         private bool _timeSelectIsVisible = true;
         private bool _timeLeftIsVisible = false;
+        private bool _startButtonIsVisible = true;
+        private bool _stopButtonIsVisible = false;
+        private bool _returnButtonIsVisible = false;
+        private bool _returnButtonEnable = false;
+
+
         private int _progress;
+        DispatcherTimer timer = new DispatcherTimer();
 
         public int Progress
         {
@@ -44,6 +52,42 @@ namespace DigitalClock.ViewModels
                 NotifyOfPropertyChange(() => TimeSelectIsVisible);
             }
         }
+        public bool StartButtonIsVisible
+        {
+            get { return _startButtonIsVisible; }
+            set
+            {
+                _startButtonIsVisible = value;
+                NotifyOfPropertyChange(() => StartButtonIsVisible);
+            }
+        }
+        public bool StopButtonIsVisible
+        {
+            get { return _stopButtonIsVisible; }
+            set
+            {
+                _stopButtonIsVisible = value;
+                NotifyOfPropertyChange(() => StopButtonIsVisible);
+            }
+        }
+        public bool ReturnButtonIsVisible
+        {
+            get { return _returnButtonIsVisible; }
+            set
+            {
+                _returnButtonIsVisible = value;
+                NotifyOfPropertyChange(() => ReturnButtonIsVisible);
+            }
+        }
+        public bool ReturnButtonEnable
+        {
+            get { return _returnButtonEnable; }
+            set
+            {
+                _returnButtonEnable = value;
+                NotifyOfPropertyChange(() => ReturnButtonEnable);
+            }
+        }
         public string Timer
         {
             get { return _timer; }
@@ -59,6 +103,10 @@ namespace DigitalClock.ViewModels
             TimeSelectIsVisible = !TimeSelectIsVisible;
             TimeLeftIsVisible = !TimeLeftIsVisible;
 
+            StartButtonIsVisible = !StartButtonIsVisible;
+            StopButtonIsVisible = !StopButtonIsVisible;
+            ReturnButtonEnable = !ReturnButtonEnable;
+
 
             Time = DateTime.ParseExact(Timer, "HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
 
@@ -69,7 +117,6 @@ namespace DigitalClock.ViewModels
 
             TimeWhenStop = DateTime.Now.AddMinutes(min).AddHours(hours).AddSeconds(sec+1);
 
-            DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += Time_Tick;
             timer.Start();
@@ -78,8 +125,59 @@ namespace DigitalClock.ViewModels
         public void Time_Tick(object sender, EventArgs args)
         {
             TimeSpan value = TimeWhenStop.Subtract(DateTime.Now);
-
             Timer = value.ToString(@"hh\:mm\:ss");
+
+            if (value.ToString(@"hh\:mm\:ss").Equals("00:00:00"))
+            {
+                manager.ShowWindowAsync(new TimerAlertViewModel(), null, null);
+                TimeSelectIsVisible = !TimeSelectIsVisible;
+                TimeLeftIsVisible = !TimeLeftIsVisible;
+                timer.Stop();
+
+                StartButtonIsVisible = !StartButtonIsVisible;
+                StopButtonIsVisible = !StopButtonIsVisible;
+            }
+
+        }
+
+        public void StopTimer()
+        {
+            timer.Stop();
+
+            ReturnButtonIsVisible = !ReturnButtonIsVisible;
+            StopButtonIsVisible = !StopButtonIsVisible;
+        }
+
+        public void ReturnTimer()
+        {
+            Time = DateTime.ParseExact(Timer, "HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+
+            int hours = Int32.Parse(Timer.ToString().Substring(0, 2));
+            int min = Int32.Parse(Timer.ToString().Substring(3, 2));
+            int sec = Int32.Parse(Timer.ToString().Substring(6, 2));
+
+
+            TimeWhenStop = DateTime.Now.AddMinutes(min).AddHours(hours).AddSeconds(sec + 1);
+
+            timer.Start();
+
+            ReturnButtonIsVisible = !ReturnButtonIsVisible;
+            StopButtonIsVisible = !StopButtonIsVisible;
+
+        }
+
+        public void Reset()
+        {
+            timer.Stop();
+            Timer = "00:00:00";
+            TimeSelectIsVisible = !TimeSelectIsVisible;
+            TimeLeftIsVisible = !TimeLeftIsVisible;
+
+            StartButtonIsVisible = true;
+            StopButtonIsVisible = false;
+            ReturnButtonIsVisible = false;
+            ReturnButtonEnable = false;
+
         }
     }
 }
